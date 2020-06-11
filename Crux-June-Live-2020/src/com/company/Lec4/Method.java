327da9fe8a0c225c5f0476765cf06d7f6b1fc2e3
ShubhamRwt <shubhamrwt02@gmail.com>
2020-06-11 23:24:35 +0530

    Lec

---
+++ Crux-June-Live-2020/src/com/company/Lec4/Method.java
@@ -0,0 +1,9 @@
+package com.company.Lec4;
+
+public class Method {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Crux-June-Live-2020/src/com/company/Lec6/Searching.java
@@ -0,0 +1,9 @@
+package com.company.Lec6;
+
+public class Searching {
+
+    public static void main(String[] args) {
+
+
+    }
+}
