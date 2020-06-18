fe38917869404c82cef5b66f7431d683e37a1d29
ShubhamRwt <shubhamrwt02@gmail.com>
2020-06-18 21:50:27 +0530

    lec

---
+++ Crux_Live_Batch_June_2020/src/info/company/Lec9/Typecasteg.java
@@ -0,0 +1,14 @@
+package info.company.Lec9;
+
+public class Typecasteg {
+
+    public static void main(String[] args) {
+
+////        int a=6;
+//
+//        long b =6;
+//        int a=(int)b;
+//
+//        System.out.println(a);
+    }
+}
