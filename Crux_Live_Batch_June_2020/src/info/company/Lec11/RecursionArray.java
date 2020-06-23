8a76fcb9ebaac4a0dac3ee7eff27fde67a2d1f96
ShubhamRwt <shubhamrwt02@gmail.com>
2020-06-23 20:37:49 +0530

    lec

--- Crux_Live_Batch_June_2020/src/info/company/Lec11/RecursionArray.java
+++ Crux_Live_Batch_June_2020/src/info/company/Lec11/RecursionArray.java
@@ -1,11 +1,19 @@
 package info.company.Lec11;
 
+import info.company.Lec6.ArratQues;
+
+import java.util.Arrays;
+
 public class RecursionArray {
 
     public static void main(String[] args) {
 
-        int[] ar= {1,2,3,4};
-        System.out.println(sortedornot(ar,0));
+//        int[] ar= {1,2,3,4};
+//        System.out.println(sortedornot(ar,0));
+        int[] ar = {4,3,2,1};
+
+        bubblesort(ar,ar.length-1,0);
+        System.out.println(Arrays.toString(ar));
     }
 
     public static boolean sortedornot(int[] ar, int index){
@@ -43,5 +51,38 @@ public class RecursionArray {
         return n + sum(n-1);
     }
 
+    public static void pattern(int row, int col){
+
+        if(row==0){
+            return;
+        }
+
+        if(col==row){
+            pattern(row-1,0);
+//            System.out.println();
+            return;
+        }
+
+        pattern(row,col+1);
+//        System.out.print("*");
+    }
+
+    public static void bubblesort(int[] ar, int i, int j){
+
+        if(i==0){
+            return;
+        }
+
+        if(j==i){
+            bubblesort(ar,i-1,0);
+            return;
+        }
+
+        if(ar[j]>ar[j+1]){
+            ArratQues.swap(j, j+1, ar);
+        }
+
+        bubblesort(ar,i,j+1);
+    }
 
 }
