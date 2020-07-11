7c05da95896a53423e9be105326bc48bf207f180
ShubhamRwt <shubhamrwt02@gmail.com>
2020-07-11 21:40:24 +0530

    lec

--- Crux_Live_Batch_June_2020/src/info/company/Lec19/QueueClient.java
+++ Crux_Live_Batch_June_2020/src/info/company/Lec19/QueueClient.java
@@ -4,18 +4,23 @@ public class QueueClient {
 
     public static void main(String[] args) {
 
-        Optimizedqueue queue = new Optimizedqueue();
-
-        for (int i = 0; i <=10 ; i++) {
-            queue.insert(i);
-        }
+        CircularQueue queue = new CircularQueue();
 
         for (int i = 0; i <10 ; i++) {
-            System.out.println(queue.delete());
-
+            queue.insert(i);
+            queue.display();
         }
 
-        System.out.println(queue.delete());
+        queue.delete();
+        queue.delete();
+        System.out.println();
+        queue.insert(15);
+        queue.insert(17);
+
+        for (int i = 0; i <10 ; i++) {
+            queue.delete();
+            queue.display();
+        }
 
 
     }
